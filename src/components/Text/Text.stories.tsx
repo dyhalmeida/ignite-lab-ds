3314691d4e148b212377e4657ff0edf10c2c75cf
Diego Almeida <diego.almeida@memed.com.br>
2022-10-12 18:44:04 -0300

    feat: add Text component

---
+++ src/components/Text/Text.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './index'
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+  args: {
+    children: 'Lorem Ipsum'
+  }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  }
+}
+
