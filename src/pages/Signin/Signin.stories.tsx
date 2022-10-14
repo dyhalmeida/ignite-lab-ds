379d6873278a40fa5d0f829428666eec3d93d6d7
Diego Almeida <diego.almeida@memed.com.br>
2022-10-14 08:38:14 -0300

    feat: add msw (mock service work) with storybook

--- src/pages/Signin/Signin.stories.tsx
+++ src/pages/Signin/Signin.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { within, userEvent, waitFor } from '@storybook/testing-library'
 import { expect } from '@storybook/jest'
+import { rest } from 'msw'
 
 import { Signin } from '.'
 
@@ -8,7 +9,20 @@ export default {
   title: 'Pages/Sign in',
   component: Signin,
   args: {},
-  argTypes: {}
+  argTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post('/sessions', (req, res, ctx) => {
+          return res(
+            ctx.json({
+              message: 'Login Success!'
+            })
+          )
+        })
+      ]
+    }
+  }
 } as Meta
 
 export const Default: StoryObj = {
